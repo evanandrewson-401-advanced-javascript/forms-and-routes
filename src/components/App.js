b264b62a80de5b56cc0ca58fbe0d761e662cebed
evanandrewson <evanandrewson@gmail.com>
2019-11-05 05:59:48 -0800

    made ListPage container

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import HomePage from '../containers/HomePage';
+import ListPage from '../containers/ListPage';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
       <Router>
         <Header />
         <Switch>
-          <Route path='/' component={HomePage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/list' component={ListPage} />
         </Switch>
         <Footer />
       </Router>
---
+++ src/containers/ListPage.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import ListOfCharacters from '../components/ListOfCharacters';
+import apiCall from '../services/apiCall';
+import PropTypes from 'prop-types';
+
+class ListPage extends Component {
+  state = {
+    characters: [],
+    searchInput: this.props.location.search.slice(6, this.props.location.search.length),
+    page: 1
+  }
+
+  componentDidMount() {
+    this.searchChars();
+  }
+
+  searchChars = () => {
+    if(this.state.searchInput) {
+      return apiCall(`http://last-airbender-api.herokuapp.com/api/v1/characters?name=${this.state.searchInput}`)
+        .then(result => {
+          this.setState({ characters: result });
+        });
+    } else {
+      return apiCall(`http://last-airbender-api.herokuapp.com/api/v1/characters?page=${this.state.page}`)
+        .then(result => {
+          this.setState({ characters: result });
+        });
+    }
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ searchInput: e.target.value });
+    
+  }
+
+  incrementPage = () => {
+    this.setState(state => ({ page: state.page + 1 }), () => {
+      return apiCall(`http://last-airbender-api.herokuapp.com/api/v1/characters?page=${this.state.page}`)
+        .then(result => {
+          this.setState({ characters: result, searchInput: '' });
+        });
+    });
+  }
+
+  decrementPage = () => {
+    this.setState(state => ({ page: state.page - 1 }), () => {
+      return apiCall(`http://last-airbender-api.herokuapp.com/api/v1/characters?page=${this.state.page}`)
+        .then(result => {
+          this.setState({ characters: result, searchInput: '' });
+        });
+    });
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    this.props.history.push(`/list/?name=${this.state.searchInput}`);
+    this.searchChars();
+  }
+
+  clearSearchInput = () => {
+    this.setState({ searchInput: null, page: 1 }, () => {
+      this.searchChars();
+    });
+    this.props.history.push('/list');
+    
+  }
+
+  render() {
+    return (
+      <>
+        <ListOfCharacters items={this.state.characters} />
+        <button disabled={this.state.searchInput ? true : false} onClick={this.incrementPage}>Page up</button>
+        <button disabled={this.state.searchInput ? true : false || this.state.page < 2} onClick={this.decrementPage}>Page down</button>
+        <button disabled={!this.state.searchInput ? true : false} onClick={this.clearSearchInput}>All Characters</button>
+        <form onSubmit={this.handleSearch}>
+          <input type="text" value={this.state.searchInput} onChange={this.handleChange}></input>
+          <button>Search</button>
+        </form>
+      </>
+    );
+  }
+}
+
+ListPage.propTypes = {
+  history: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired
+};
+
+export default ListPage;
