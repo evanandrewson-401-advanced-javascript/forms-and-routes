17134e89e4b92773ade241a94476cd1300af1b1d
evanandrewson <evanandrewson@gmail.com>
2019-11-05 07:32:20 -0800

    added nav links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <h1>Avatar characters</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/list">List of Characters</Link>
+      </nav>
     </header>
   );
 };
